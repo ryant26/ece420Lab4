4481659881d3eb0a3ef4287e45fbc788a510880a
ryant26 <ryant26@gmail.com>
2016-03-03 16:06:27 -0700

    beggining process responsibilities

--- src/main.c
+++ src/main.c
@@ -6,11 +6,20 @@
 #include "../sdk/timer.h"
 #include <mpi.h>
 
+#define MASTER		0
+
 int main(int argc, char * argv[]){
-	int threads;
+	int threads, rank;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &threads);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (rank == MASTER){
+		printf("Hello from master, there are %d processes: \n", threads);
+	} else {
+		printf("Hello from: %d\n", rank);
+	}
 	MPI_Finalize();
 	return 0;
 }
